ba42e5f40aab671143f7093cf3d1d8db7207a7ba
Isaí Dzul <99771519+Isaidzp@users.noreply.github.com>
2022-03-15 00:35:55 -0600

    Add files via upload

--- 11_1-clienteserver.py
+++ 11_1-clienteserver.py
@@ -10,16 +10,17 @@ server_port = 9999
 s.connect((server_host, server_port))
 
 respuesta = s.recv(4096).decode()
-print(f'{respuesta}')
+print(f'{respuesta}') #recibe si es jugador 1 o 2
 
 # print('Elija su opcion: [piedra, papel o tijera]')
 while True:
-    mensaje = input()
-    if mensaje != 'adios': 
-        s.send(mensaje.encode())
-        respuesta = s.recv(4096).decode()
-        print(f'SERVIDOR: {respuesta}')
+    mensaje = input() #espera a que el jugador elija su opcion
+    if mensaje != 'salir': 
+        s.send(mensaje.encode()) #envia la opcion al servidor
+        respuesta = s.recv(4096).decode() #espera la respuesta del servidor
+        print(f'SERVIDOR: {respuesta}') #el servidor responde si ganó o perdió la partida
     else: #si el mensaje es adios, lo envía al servidor y termina la conexion
         s.send(mensaje.encode())
         s.close()
         sys.exit()
+    
--- 11_1-serverTCP.py
+++ 11_1-serverTCP.py
@@ -9,12 +9,11 @@ s.bind((host, port))
 print('Esperando conexion')
 s.listen(2)
 
-
 while True:
     conn, addr = s.accept() #establecemos la conexion
     print('Jugador 1 ¡CONECTADO!')
     print(f'Direccion: {addr}')
-    conn.send('J1'.encode())#envía un mensaje fijo#envía un mensaje fijo
+    conn.send('J1'.encode())#envía un mensaje fijo#
     print('Esperando segundo jugador')
     conn2, addr2 = s.accept() #establecemos la conexion
     print('Jugador 2 ¡CONECTADO!')
@@ -22,12 +21,28 @@ while True:
     conn2.send('J2'.encode())#envía un mensaje fijo
     
     while True:
-        
         mensajeRecibido1 = conn.recv(4096).decode() #recibe el mensaje del jugador 1
-        print(f'Jugador 1: {mensajeRecibido1}')        
-        mensajeRecibido2 = conn2.recv(4096).decode() #recibe el mensaje del Jugador 2
+        print(f'Jugador 1: {mensajeRecibido1}')
+        if mensajeRecibido1 == 'salir': #si el jugador 1 se desconecta
+            print('Esperando primer jugador')
+            conn, addr = s.accept() #establecemos la conexion
+            print('Jugador 1 ¡CONECTADO!')
+            print(f'Direccion: {addr}')  
+            conn.send('J1'.encode())#envía un mensaje fijo#
+            mensajeRecibido1 = conn.recv(4096).decode() #recibe el mensaje del jugador 1
+            print(f'Jugador 1: {mensajeRecibido1}')
+        
+        mensajeRecibido2 = conn2.recv(4096).decode() #recibe el mensaje del jugador 2
         print(f'Jugador 2: {mensajeRecibido2}')
-
+        if mensajeRecibido2 == 'salir': #si el jugador 2 se desconecta
+            print('Esperando segundo jugador')
+            conn2, addr2 = s.accept() #establecemos la conexion
+            print('Jugador 2 ¡CONECTADO!')
+            print(f'Direccion: {addr2}')  
+            conn2.send('J2'.encode())#envía un mensaje fijo#
+            mensajeRecibido2 = conn2.recv(4096).decode() #recibe el mensaje del jugador 2
+            print(f'Jugador 2: {mensajeRecibido2}')
+    
         if mensajeRecibido1 == mensajeRecibido2:    
             conn.send('Empate'.encode()) #se envía mensaje al jugador 1
             conn2.send('Empate'.encode()) #se envía mensaje al jugador 2
@@ -52,26 +67,9 @@ while True:
         elif mensajeRecibido1 == 'tijera' and mensajeRecibido2 == 'papel': 
             conn.send('Ganaste'.encode()) #se envía mensaje al jugador 1
             conn2.send('Perdiste'.encode()) #se envía mensaje al jugador 2
-    
-    #     continuar = input("¿Quiere seguir jugando? (s/n): ")
-    #     if continuar=="n":
-    #         break
-
-    # print("Vuelva pronto")
-            
-
-        # if mensajeRecibido1 == 'adios': #si el mensaje es un adios, le envia un mensaje de hasta pronto jugador al cliente 1
-        #     print('Hasta pronto Jugador 1')
-        #     break
-        # conn.send(input().encode()) #envía al cliente el mensaje ingresado por teclado
-        
-        # if mensajeRecibido2 == 'adios':
-        #     print('Hasta pronto Jugador 2')
-        #     break
-        # conn2.send(input().encode()) #envía al cliente el mensaje ingresado por teclado
-    
+  
     # print(f'Desconectando al cliente 1: {addr}')
     # print(f'Desconectando al cliente 2: {addr2}')
 
-    conn.close()
-    conn2.close
+conn.close()
+conn2.close
